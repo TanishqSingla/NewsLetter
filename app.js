b7b6ce7e9da2a03280fa51a9d21181e1eca738a3
TanishqSingla <tanishqsingla00@gmail.com>
2020-05-05 16:26:14 +0530

    Getting user input data

--- app.js
+++ app.js
@@ -4,10 +4,23 @@ const request = require("request");
 
 const app = express();
 
+//Using body parser
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Serving up styles and images
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/signup.html");
+});
+
+//Getting form input
+app.post("/", (req, res) => {
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+
+  console.log(firstName, lastName, email);
 });
 
 app.listen(3000, () => {

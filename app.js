fd81bc198f3875810dcc2888646a00e69eb5a966
TanishqSingla <tanishqsingla00@gmail.com>
2020-05-05 15:55:03 +0530

    Setting up server
    Added express and setup server
    added body-parser
    added request

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Server Up and running");
+});
+
+app.listen(3000, () => {
+  console.log("Server is live at port 3000");
+});
